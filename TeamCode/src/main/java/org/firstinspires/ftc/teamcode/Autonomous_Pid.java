b3ff104372eb618551ad1a3324e1f75e3140cfd7
gabriel <sesiftc22936@gmail.com>
2026-04-01 16:54:53 -0300

    01/04/2026 - Proportional done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Pid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Pid.java
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@Autonomous
 public class Autonomous_Pid extends LinearOpMode {
     private DcMotor dd,de,td,te;
-    private double kp = 0.5;//verificar no robo
-    double odometria = 0;
+    GoBildaPinpointDriver pinpointer;
+    private double kp = 0.000072;//verificar no robo
     @Override
     public void runOpMode() throws InterruptedException {
         dd = hardwareMap.get(DcMotor.class, "dd");
@@ -15,17 +20,37 @@ public class Autonomous_Pid extends LinearOpMode {
         td = hardwareMap.get(DcMotor.class, "td");
         te = hardwareMap.get(DcMotor.class, "te");
 
+        pinpointer = hardwareMap.get(GoBildaPinpointDriver.class, "pn");
+
         de.setDirection(DcMotorSimple.Direction.REVERSE);
         te.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        pinpointer.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+        pinpointer.setEncoderDirections(
+                GoBildaPinpointDriver.EncoderDirection.FORWARD,
+                GoBildaPinpointDriver.EncoderDirection.REVERSED
+        );// x,y
+
+        pinpointer.resetPosAndIMU();
+
+        pinpointer.update();
+        final double lastX = pinpointer.getEncoderX();
+        final double lastY = pinpointer.getEncoderY();
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
         while (opModeIsActive()){
+            pinpointer.update();
+
+            double x = pinpointer.getEncoderX() - lastX;
+            double y = pinpointer.getEncoderY() - lastY;
+            double z = pinpointer.getHeading(AngleUnit.DEGREES);
+
             double setpoint = 1000;
 
-            double error = odometria - setpoint;
+            double error = setpoint - y;
             double speed = kp * error;
 
             dd.setPower(speed);
@@ -33,7 +58,10 @@ public class Autonomous_Pid extends LinearOpMode {
             td.setPower(speed);
             te.setPower(speed);
 
-            telemetry.addData("Odometria X",odometria);
+            telemetry.addData("Odometria Y",y);
+            telemetry.addData("Odometria X",x);
+            telemetry.addData("Odometria Z",z);
+            telemetry.update();
         }
     }
 }
